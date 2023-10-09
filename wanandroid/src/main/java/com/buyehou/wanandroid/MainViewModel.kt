59d997033a48fb3a85d0fd355d2a075b945e1e37
Rosen <rosen@txzing.com>
2023-10-09 12:29:04 +0800

    添加login

---
+++ wanandroid/src/main/java/com/buyehou/wanandroid/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.buyehou.wanandroid
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Rosen
+ * @date 2023/10/9 11:34
+ */
+class MainViewModel : ViewModel() {
+
+    val username = MutableLiveData("")
+
+}
